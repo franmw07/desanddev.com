23548ed8e233ce9eccb21b00c72839f493fb9f6c
francisco.martinez <francisco.martinez@worksut.com>
2025-11-19 23:37:39 +0100

    update url img

--- src/data/skills.js
+++ src/data/skills.js
@@ -2,45 +2,45 @@ export const skills = {
     diseño: [
     { 
       nombre: { es: "Adobe Illustrator", en: "Adobe Illustrator" }, 
-      logo: "/src/assets/logos/Adobe_Illustrator.png" 
+      logo: "../../assets/logos/Adobe_Illustrator.png" 
     },
     { 
       nombre: { es: "Adobe Photoshop", en: "Adobe Photoshop" }, 
-      logo: "/src/assets/logos/adobe-photoshop.png" 
+      logo: "../../assets/logos/adobe-photoshop.png" 
     },
     { 
       nombre: { es: "Adobe XD", en: "Adobe XD" }, 
-      logo: "/src/assets/logos/Adobe-xd.webp" 
+      logo: "../../assets/logos/Adobe-xd.webp" 
     },
     { 
       nombre: { es: "Adobe After Effects", en: "Adobe After Effects" }, 
-      logo: "/src/assets/logos/adobe-after-effects.webp" 
+      logo: "../../assets/logos/adobe-after-effects.webp" 
     },
     { 
       nombre: { es: "Adobe Premiere Pro", en: "Adobe Premiere Pro" }, 
-      logo: "/src/assets/logos/adobe-premier-pro.webp" 
+      logo: "../../assets/logos/adobe-premier-pro.webp" 
     },
   ],
   desarrollo: [
     { 
       nombre: { es: "HTML5", en: "HTML5" }, 
-      logo: "/src/assets/logos/html5.png" 
+      logo: "../../assets/logos/html5.png" 
     },
     { 
       nombre: { es: "CSS3", en: "CSS3" }, 
-      logo: "/src/assets/logos/CSS3.webp" 
+      logo: "../../assets/logos/CSS3.webp" 
     },
     { 
       nombre: { es: "JavaScript", en: "JavaScript" }, 
-      logo: "/src/assets/logos/js.webp" 
+      logo: "../../assets/logos/js.webp" 
     },
     { 
       nombre: { es: "jQuery", en: "jQuery" }, 
-      logo: "/src/assets/logos/jq.webp" 
+      logo: "../../assets/logos/jq.webp" 
     },
     { 
       nombre: { es: "React", en: "React" }, 
-      logo: "/src/assets/logos/react.webp" 
+      logo: "../../assets/logos/react.webp" 
     },
   ],
   habilidades: [
